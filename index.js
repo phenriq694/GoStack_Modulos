e996922130972becc0f1773e580b4383725eac06
Paulo Henrique <phenriq694@gmail.com>
2019-10-14 14:50:25 -0300

    Query & Route params

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express')
 
 const server = express()
 
-server.get('/teste', (req, res) => {
-  return res.json({ message: 'Hello World'})
+// Query params = ?nome=Paulo
+// Route params = /users/1
+// Request body = {"name": "Diego", "email":"diego@rocketseat.com.br"}
+
+server.get('/users', (req, res) => {
+  const nome = req.query.nome 
+
+  return res.json({ message : `Hello ${nome}`})
+})
+
+server.get('/users/:id', (req, res) => {
+  const id = req.params.id 
+
+  return res.json({ message : `Buscando o id ${id}`})
 })
 
 server.listen(3000)
