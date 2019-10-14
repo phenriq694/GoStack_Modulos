e701f48bf9dd7947d673a6d0f68a602d32e049b2
Paulo Henrique <phenriq694@gmail.com>
2019-10-14 15:18:21 -0300

    CRUD

--- index.js
+++ index.js
@@ -2,22 +2,49 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.json())
+
 // Query params = ?nome=Paulo
 // Route params = /users/1
 // Request body = {"name": "Diego", "email":"diego@rocketseat.com.br"}
 
-server.get('/users', (req, res) => {
-  const nome = req.query.nome 
-
-  return res.json({ message : `Hello ${nome}`})
-})
+//CRUD - Create, Read, Update, Delete
 
 const users = ['Diego', 'Cláudio', 'Victor']
+
+server.get('/users', (req, res) => {
+  return res.json(users)
+})
 
 server.get('/users/:index', (req, res) => {
   const { index } = req.params
 
   return res.json(users[index])
+})
+
+server.post('/users', (req, res) => {
+  const { name } = req.body
+
+  users.push(name)
+
+  return res.json(users)
+})
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params 
+  const { name } = req.body
+
+  users[index] = name
+
+  return res.json(users)
+})
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params
+
+  users.splice(index, 1)
+
+  return res.send()
 })
 
 server.listen(3000)
