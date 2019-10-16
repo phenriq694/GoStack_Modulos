ce1088845d9595aa94e36035f3d1844f60115e83
Paulo Henrique <phenriq694@gmail.com>
2019-10-16 09:01:19 -0300

    MODULO 02: Cadastro de usuários

--- modulo02/gobarber/src/routes.js
+++ modulo02/gobarber/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Paulo Henrique',
+    email: 'phenriq@gmail.com',
+    password_hash: 'q32545234524',
+  });
+
+  res.json(user);
+});
 
 export default routes;
