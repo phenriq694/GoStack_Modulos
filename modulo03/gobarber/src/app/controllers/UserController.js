ab0f9964792fe2ef6a5ceaaa4c840dd9768176b2
Paulo Henrique <phenriq694@gmail.com>
2019-10-17 18:37:19 -0300

    MODULO 03: Configuração de relacionamento entre as models File e User para adicionar uma chave estrangeira da tabela 'files' dentro da tabela 'users'

--- modulo03/gobarber/src/app/controllers/UserController.js
+++ modulo03/gobarber/src/app/controllers/UserController.js
@@ -56,11 +56,13 @@ class UserController {
 
     const user = await User.findByPk(req.userId);
 
-    if (email !== user.email) {
-      const userExists = await User.findOne({ where: { email } });
+    if (email) {
+      if (email !== user.email) {
+        const userExists = await User.findOne({ where: { email } });
 
-      if (userExists) {
-        return res.status(400).json({ error: 'User already exists' });
+        if (userExists) {
+          return res.status(400).json({ error: 'User already exists' });
+        }
       }
     }
 
