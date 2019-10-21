57553e94ddccedf692772f481cade64597ce128f
Paulo Henrique <phenriq694@gmail.com>
2019-10-21 16:47:15 -0300

    MODULO 03: Configuração do módulo Nodemailer para envio de e-mails quando um cancelamento é efetuado.

---
+++ modulo03/gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '147a6529fe85ee',
+    pass: 'ceabb313d6d278',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
