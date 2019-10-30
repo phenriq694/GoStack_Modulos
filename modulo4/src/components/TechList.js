6a61fa6b0ef67c3e5d681282f76c6526972ff648
Paulo Henrique <phenriq694@gmail.com>
2019-10-30 12:30:52 -0300

    MODULO 04: Removendo itens do estados.

--- modulo4/src/components/TechList.js
+++ modulo4/src/components/TechList.js
@@ -20,11 +20,24 @@ class TechList extends Component {
     this.setState({techs: [...this.state.techs, this.state.newTech], newTech: ''})
   }
 
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>      
         <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button
+              onClick={() => this.handleDelete(tech)}
+              type="button">
+                Remover
+              </button>
+            </li>
+          ))}
         </ul>
         <input 
           type="text" 
