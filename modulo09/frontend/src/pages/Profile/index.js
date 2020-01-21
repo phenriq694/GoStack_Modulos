35e6a50ec5de94cd2d6d3de2f82a6ee2a7ec2f45
Paulo Henrique <phenriq694@gmail.com>
2020-01-21 11:08:25 -0300

    MODULO 09: FRONTEND - LogOut da aplicação.

--- modulo09/frontend/src/pages/Profile/index.js
+++ modulo09/frontend/src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -16,6 +17,10 @@ export default function Profile() {
     dispatch(updateProfileRequest(data));
   }
 
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
@@ -28,7 +33,7 @@ export default function Profile() {
 
         <Input
           type="password"
-          name="oldPassaword"
+          name="oldPassword"
           placeholder="Sua senha atual"
         />
         <Input
@@ -45,7 +50,9 @@ export default function Profile() {
         <button type="submit">Atualizar Perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
