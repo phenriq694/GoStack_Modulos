73b2b98a2ed7481e666c4a090e8a3df8a44738cc
Paulo Henrique <phenriq694@gmail.com>
2020-04-03 18:21:44 -0300

    MODULO 04: Adicionando o desafio que estava em outro repositório.

---
+++ modulo04/Desafio/Introdução ao React/README.md
@@ -0,0 +1,3 @@
+Desafio do Módulo 4
+
+Proposta do desafio: https://github.com/Rocketseat/bootcamp-gostack-desafio-04
---
+++ modulo04/Desafio/Introdução ao React/src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import logo from '../assets/logo.png'
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src={logo}/>
+        <div>
+          <span>Meu Perfil</span>
+          <i className="material-icons">account_circle</i>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
