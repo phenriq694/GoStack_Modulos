b91bbfce30b666ff3457ba69de8d994eef9509b8
Paulo Henrique <phenriq694@gmail.com>
2020-04-03 18:58:59 -0300

    MODULO 05: Adicionando o desafio que estava em outro repositório.

---
+++ modulo05/Desafio/src/pages/Repository/index.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { Loading, Owner, IssueList, IssueFilter, PageChange } from './styles';
+
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+    filters: [
+      { state: 'all', name: 'Todos', active: true },
+      { state: 'open', name: 'Abertos', active: false },
+      { state: 'closed', name: 'Fechados', active: false },
+    ],
+    filterIndex: 0,
+    page: 1,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const { filters } = this.state;
+
+    const filterActive = filters.find(f => f.active);
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: filterActive.state,
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  }
+
+  loadIssues = async () => {
+    const { match } = this.props;
+    const { filters, filterIndex, page } = this.state;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: filters[filterIndex].state,
+          per_page: 5,
+          page,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  };
+
+  handleFilterClick = async filterIndex => {
+    await this.setState({ filterIndex });
+    this.loadIssues();
+  };
+
+  handleChangePage = async action => {
+    const { page } = this.state;
+    await this.setState({
+      page: action === 'back' ? page - 1 : page + 1,
+    });
+    this.loadIssues();
+  };
+
+  render() {
+    const {
+      repository,
+      issues,
+      loading,
+      filters,
+      filterIndex,
+      page,
+    } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando...</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssueList>
+          <IssueFilter active={filterIndex}>
+            {filters.map((filter, index) => (
+              <button
+                type="button"
+                onClick={() => this.handleFilterClick(index)}
+              >
+                {filter.name}
+              </button>
+            ))}
+          </IssueFilter>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+
+        <PageChange>
+          <button
+            disabled={page === 1}
+            type="button"
+            onClick={() => this.handleChangePage('back')}
+          >
+            Anterior
+          </button>
+
+          <span>Página {page}</span>
+
+          <button type="button" onClick={() => this.handleChangePage('next')}>
+            Próxima
+          </button>
+        </PageChange>
+      </Container>
+    );
+  }
+}
