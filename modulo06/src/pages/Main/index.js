e972a0d9a2f81f1a69dce81106f337a011fedc4c
Paulo Henrique <phenriq694@gmail.com>
2020-04-06 19:12:10 -0300

    MODULO 06: Configuração da biblioteca react-native-vector-icons.

--- modulo06/src/pages/Main/index.js
+++ modulo06/src/pages/Main/index.js
@@ -3,7 +3,19 @@ import React from 'react';
 import { Container } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+
+        <SubmitButton />
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
