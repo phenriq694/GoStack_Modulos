448e336c638cbed68745d4da8d4b67f003a80adb
Paulo Henrique <phenriq694@gmail.com>
2020-04-21 03:07:11 -0300

    LEVEL O2 - FIRST-PROJECT-WITH-NODEJS: SOLID & Services

--- level02/first-project-with-nodejs/src/routes/appointments.routes.ts
+++ level02/first-project-with-nodejs/src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentRepository from '../respositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppoitmentService';
 
 const appointmentRouter = Router();
 const appointmentRepository = new AppointmentRepository();
@@ -13,26 +14,24 @@ appointmentRouter.get('/', (request, response) => {
 });
 
 appointmentRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentRepository.findByDate(
-    parsedDate,
-  );
+    const createAppointment = new CreateAppointmentService(
+      appointmentRepository,
+    );
 
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appointment is already booked' });
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentRepository.create({
-    provider,
-    date: parsedDate,
-  });
-
-  return response.json(appointment);
 });
 
 export default appointmentRouter;
