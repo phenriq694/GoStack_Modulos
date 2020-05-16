7e6a363f738007302172cd816580b70c5f272604
Paulo Henrique <phenriq694@gmail.com>
2020-05-16 17:12:00 -0300

    LEVEL 02 - GOBARBER-BACKEND: Relationship in models

--- level02/gobarber-backend/src/database/migrations/1588463978787-CreateUsers.ts
+++ level02/gobarber-backend/src/database/migrations/1588463978787-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588463978787 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
