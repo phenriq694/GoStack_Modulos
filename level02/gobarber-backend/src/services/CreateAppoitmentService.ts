e4c168be1701dbf51f8b82fd969a839c543ff322
Paulo Henrique <phenriq694@gmail.com>
2020-05-21 21:42:31 -0300

    Create Error Class

--- level02/gobarber-backend/src/services/CreateAppoitmentService.ts
+++ level02/gobarber-backend/src/services/CreateAppoitmentService.ts
@@ -1,6 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentRepository from '../respositories/AppointmentRepository';
 
@@ -20,7 +22,7 @@ class CreateAppointmetService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked');
+      throw new AppError('This appointment is already booked');
     }
 
     const appointment = appointmentRepository.create({
