172bead5231d86c35826db9c6f635bb0ae5cede6
Paulo Henrique <phenriq694@gmail.com>
2020-07-25 00:17:51 -0300

    MODULE 12 - FRONTEND: Add a test for form element

--- module12/frontend/src/__tests__/components/TechList.test.js
+++ module12/frontend/src/__tests__/components/TechList.test.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react';
 
-import TechList from '~/components/TechList'
+import TechList from '~/components/TechList';
 
 describe('TechList component', () => {
   it('should be able to add new tech', () => {
-    const { getByText, getByTestId, debug } = render(<TechList />);
+    const { getByText, getByTestId, getByLabelText, debug} = render(<TechList />);
 
-    debug();
-
-    fireEvent.click(getByText('Adicionar'));
-
-    debug();
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    fireEvent.submit(getByTestId('tech-form'));
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    expect(getByLabelText('Tech')).toHaveValue('');
   });
 });
