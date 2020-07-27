347ac00b665bb37e4fb65808f8caf0bc8bed55bc
Paulo Henrique <phenriq694@gmail.com>
2020-07-26 23:50:00 -0300

    MODULE 12 - FRONTEND: Add test coverage

--- module12/frontend/src/__tests__/store/reducers/items.test.js
+++ module12/frontend/src/__tests__/store/reducers/items.test.js
@@ -2,6 +2,12 @@ import reducers, { INITIAL_STATE } from '~/store/modules/items/reducer';
 import * as Items from '~/store/modules/items/actions';
 
 describe('Items reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducers(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
+  });
+
   it('ADD_ITEM', () => {
     const state = reducers(INITIAL_STATE, Items.addItem('Notebook'));
 
