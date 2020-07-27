4d5635995376e1d57feed6fd95af231cf5979728
Paulo Henrique <phenriq694@gmail.com>
2020-07-26 23:04:33 -0300

    MODULE 12 - FRONTEND: Add axios mock in the saga test

--- module12/frontend/src/__tests__/store/sagas/items.test.js
+++ module12/frontend/src/__tests__/store/sagas/items.test.js
@@ -1,14 +1,30 @@
 import { runSaga } from 'redux-saga';
+import MockAdapter from 'axios-mock-adapter';
+import api from '~/services/api';
 
-import { getItemsSuccess } from '~/store/modules/items/actions';
+import { getItemsSuccess, getItemsFailure } from '~/store/modules/items/actions';
 import { getItems } from '~/store/modules/items/sagas';
+
+const apiMock = new MockAdapter(api);
 
 describe('Items Saga', () => {
   it('should be able to fetch items', async () => {
     const dispatch = jest.fn();
 
+    apiMock.onGet('items').reply(200, ['Notebook']);
+
     await runSaga({ dispatch }, getItems).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(getItemsSuccess(['Notebook']));
+  });
+
+  it('should fail when api returns error', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet('items').reply(500);
+
+    await runSaga({ dispatch }, getItems).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getItemsFailure());
   });
 });
