5e3b059b64519b1059eb02b062e5f357dca59063
Paulo Henrique <phenriq694@gmail.com>
2020-07-30 23:14:59 -0300

    MODULE 14: Add helmet

--- module14/Node Security/src/app.js
+++ module14/Node Security/src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
+import helmet from 'helmet';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import sentryConfig from './config/sentry';
@@ -24,6 +25,7 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(helmet());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
