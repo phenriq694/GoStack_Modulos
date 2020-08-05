545dd41bd84c764cf048fbcdf1c0f1667b1a55c7
Paulo Henrique <phenriq694@gmail.com>
2020-08-05 00:04:38 -0300

    LEVEL 03 - FIRST-REACT-PROJECT: Styling Dashboard

--- level03/first-react-project/src/pages/Dashboard/index.tsx
+++ level03/first-react-project/src/pages/Dashboard/index.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars2.githubusercontent.com/u/54601930?s=60&v=4"
+            alt="Paulo Henrique"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalate ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="#">
+          <img
+            src="https://avatars2.githubusercontent.com/u/54601930?s=60&v=4"
+            alt="Paulo Henrique"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalate ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
