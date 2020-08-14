f7868ad3042d21a2a9f3e0e19f2d755323e7f3b3
Paulo Henrique <phenriq694@gmail.com>
2020-08-14 02:56:15 -0300

    LEVEL 04 - ARCHITECTURE-TESTING: Add controllers

--- level04/gobarber-backend/src/modules/user/infra/http/routes/sessions.routes.ts
+++ level04/gobarber-backend/src/modules/user/infra/http/routes/sessions.routes.ts
@@ -1,23 +1,10 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
 
-import AuthenticateUsersService from '@modules/user/services/AuthenticateUsersService';
+import SessionsController from '../controllers/SessionController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUser = container.resolve(AuthenticateUsersService);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
--- level04/gobarber-backend/src/modules/user/infra/http/routes/users.routes.ts
+++ level04/gobarber-backend/src/modules/user/infra/http/routes/users.routes.ts
@@ -1,47 +1,23 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { container } from 'tsyringe';
 
-import CreateUserService from '@modules/user/services/CreateUserService';
-import UpdateUserAvatarService from '@modules/user/services/UpdateUserAvatarService';
 import uploadConfig from '@config/upload';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-  const { name, email, password } = request.body;
-
-  const createUser = container.resolve(CreateUserService);
-
-  const user = await createUser.execute({
-    name,
-    email,
-    password,
-  });
-
-  delete user.password;
-
-  return response.json(user);
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
   upload.single('avatar'),
-  async (request, response) => {
-    const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      avatarFilename: request.file.filename,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-  },
+  userAvatarController.update,
 );
 export default usersRouter;
