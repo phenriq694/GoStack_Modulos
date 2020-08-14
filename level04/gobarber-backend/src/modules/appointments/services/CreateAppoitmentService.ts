5f7a4acab78a6416fa61c492bd81c96dd79f580a
Paulo Henrique <phenriq694@gmail.com>
2020-08-13 21:14:57 -0300

    LEVEL 04 - ARCHITECTURE-TESTING: Copy GoBarber API from level02 and separating the structure in modules

---
+++ level04/gobarber-backend/src/modules/appointments/services/CreateAppoitmentService.ts
@@ -0,0 +1,39 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Appointment from '../models/Appointment';
+import AppointmentRepository from '../respositories/AppointmentRepository';
+
+interface Request {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmetService {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(AppointmentRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = appointmentRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmetService;
---
+++ level04/gobarber-backend/src/shared/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentRepository from '../modules/appointments/repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppoitmentService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const appointmentRouter = Router();
+
+appointmentRouter.use(ensureAuthenticated);
+
+appointmentRouter.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentRepository);
+
+  const appointments = await appointmentRepository.find();
+
+  return response.json(appointments);
+});
+
+appointmentRouter.post('/', async (request, response) => {
+  const { provider_id, date } = request.body;
+
+  const parsedDate = parseISO(date);
+
+  const createAppointment = new CreateAppointmentService();
+
+  const appointment = await createAppointment.execute({
+    provider_id,
+    date: parsedDate,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentRouter;
