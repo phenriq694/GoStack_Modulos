5fa1434ca2a74d53c8e3552f89d5a23cfbfb476d
Paulo Henrique <phenriq694@gmail.com>
2020-08-14 20:56:18 -0300

    LEVEL 04 - ARCHITECTURE-TESTING: Add test to CreateAppointmnetsService

--- level04/gobarber-backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ level04/gobarber-backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,3 +1,19 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentsService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456');
+  });
 });
