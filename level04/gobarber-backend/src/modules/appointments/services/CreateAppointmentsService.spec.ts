1d52211e60d07ce792b03ee38b81a9693555803f
Paulo Henrique <phenriq694@gmail.com>
2020-08-14 22:03:18 -0300

    LEVEL 04 - ARCHITECTURE-TESTING: Add new CreateAppointmnet test

--- level04/gobarber-backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ level04/gobarber-backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,5 +1,7 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentsService from './CreateAppointmentService';
+import appointmentRouter from '../infra/http/routes/appointments.routes';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
@@ -15,5 +17,26 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123456');
+  });
+
+  it('should not be able to create two appointment on the same date', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 8, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123456',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
