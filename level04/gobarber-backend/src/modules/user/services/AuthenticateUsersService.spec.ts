90950689620f512c692b61cd2d9dca6c4af22649
Paulo Henrique <phenriq694@gmail.com>
2020-08-15 01:08:56 -0300

    LEVEL 04 - ARCHITECTURE-TESTING: Isolate password hash feature

--- level04/gobarber-backend/src/modules/user/services/AuthenticateUsersService.spec.ts
+++ level04/gobarber-backend/src/modules/user/services/AuthenticateUsersService.spec.ts
@@ -2,12 +2,18 @@ import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUserRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUsersService from './AuthenticateUsersService';
 import CreateUsersService from './CreateUserService';
 
-describe('CreateUser', () => {
-  it('should be able to create a new user', async () => {
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUsersService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
     const createUser = new CreateUsersService(
       fakeUsersRepository,
@@ -20,30 +26,12 @@ describe('CreateUser', () => {
       password: '123456',
     });
 
-    expect(user).toHaveProperty('id');
-  });
-
-  it('should not be able to create a new user with same email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-
-    const createUser = new CreateUsersService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
-    await createUser.execute({
-      name: 'John Doe',
+    const response = await authenticateUser.execute({
       email: 'johndoe@example.com',
       password: '123456',
     });
 
-    expect(
-      createUser.execute({
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: '123456',
-      }),
-    ).rejects.toBeInstanceOf(AppError);
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
   });
 });
