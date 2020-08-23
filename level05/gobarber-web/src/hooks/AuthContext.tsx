4c82cdc9c34801c355ee7855ea56194313e9a2c1
Paulo Henrique <phenriq694@gmail.com>
2020-08-22 21:54:43 -0300

    LEVEL 05 - FINISHING-GOBARBER-WEB: Create Dashboard Header

--- level05/gobarber-web/src/hooks/AuthContext.tsx
+++ level05/gobarber-web/src/hooks/AuthContext.tsx
@@ -1,9 +1,15 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCreadentials {
@@ -12,7 +18,7 @@ interface SignInCreadentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   signIn(credentials: SignInCreadentials): Promise<void>;
   signOut(): void;
 }
