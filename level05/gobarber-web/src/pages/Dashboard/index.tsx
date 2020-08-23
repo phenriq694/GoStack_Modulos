802fa45343dc35be2e7abdae3cfac4044ec27c4a
Paulo Henrique <phenriq694@gmail.com>
2020-08-23 20:11:13 -0300

    LEVEL 05 - FINISHING-GOBARBER-WEB: Create Profile page

--- level05/gobarber-web/src/pages/Dashboard/index.tsx
+++ level05/gobarber-web/src/pages/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import 'react-day-picker/lib/style.css';
 import { format, isToday, isAfter, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { FiPower, FiClock } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import {
   Container,
@@ -149,7 +150,9 @@ const Dashboard: React.FC = () => {
             />
             <div>
               <span>Bem-vindo</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
